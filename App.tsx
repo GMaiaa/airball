ad1a9b8b688944be41204434314120990fa03bf9
yuriTakamisawaRibeiro <yuri.takamisawa.ribeiro@gmail.com>
2024-11-22 10:20:38 -0300

    update menu

--- App.tsx
+++ App.tsx
@@ -10,10 +10,6 @@ import CourtGames from '@screens/CourtGames';
 import { SignIn } from '@screens/SignIn';
 import { Routes } from '@routes/index';
 import { AuthContextProvider } from '@contexts/AuthContext';
-import Contact from '@screens/Contact';
-import { NavigationContainer } from '@react-navigation/native';
-import { AboutUs } from '@screens/AboutUs';
-import { TermsAndConditions } from '@screens/TermsAndConditions';
 
 
 
@@ -28,10 +24,7 @@ export default function App() {
         translucent
       />
       <AuthContextProvider>
-        <NavigationContainer>
-
-        {fontsLoaded ? <AboutUs/> : <Loading />}
-        </NavigationContainer>
+        {fontsLoaded ? <Routes /> : <Loading />}
       </AuthContextProvider>
     </ThemeProvider>
   );
